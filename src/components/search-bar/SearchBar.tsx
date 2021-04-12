f3176c154b963c921991600f873f2251934930b3
Tony <tony.d.nguyen@protonmail.com>
2021-04-12 11:24:22 -0500

    Created getBalanceSheet function in searchbar.

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -49,6 +49,27 @@ const SearchBar = ({ searchProps }: SearchBarInterface): JSX.Element => {
         });
   }
 
+  const getBalanceSheet = (): void => {
+    axios
+        .get(
+          `${alphaVantageAPI.base}function=BALANCE_SHEET&symbol=${query}&apikey=${alphaVantageAPI.key}`
+        )
+        .then((response: AxiosResponse<any>) => {
+          searchProps(response.data);
+          console.log(response.data);
+        })
+        .catch((error: any) => {
+          // Error if city name was not found or invalid input.
+          if (error.response) console.log(error.response.data);
+          
+          // Error if no response was received.
+          if (error.request) console.log(error.request);
+
+          // Other errors.
+          console.log(error.message);
+        });
+  }
+
   return (
     <div className="search-box">
       <input
