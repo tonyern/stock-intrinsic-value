8ef3f408dad3c9bb7105c5979c4b4540e749e37c
Tony <tony.d.nguyen@protonmail.com>
2021-04-12 15:07:05 -0500

    Added test cases for the recent changes.

--- src/components/search-bar/__test__/SearchBar.test.tsx
+++ src/components/search-bar/__test__/SearchBar.test.tsx
@@ -5,14 +5,27 @@ import SearchBar from "../SearchBar";
 
 it("Search bar renders without crashing", () => {
   const div = document.createElement("div");
-  const searchProps = jest.fn();
-  ReactDOM.render(<SearchBar searchProps={searchProps} />, div);
+  const overviewProps = jest.fn();
+  const balanceSheetProps = jest.fn();
+  const cashFlowProps = jest.fn();
+  const incomeStatementProps = jest.fn();
+  ReactDOM.render(<SearchBar 
+    overviewProps={overviewProps}
+    balanceSheetProps={balanceSheetProps}
+    cashFlowProps={cashFlowProps}
+    incomeStatementProps={incomeStatementProps} />, div);
 });
 
 it("Renders with placeholder text correctly", () => {
-  const searchProps = jest.fn();
+  const overviewProps = jest.fn();
+  const balanceSheetProps = jest.fn();
+  const cashFlowProps = jest.fn();
+  const incomeStatementProps = jest.fn();
   const { queryByTestId, queryByPlaceholderText } = render(<SearchBar 
-    searchProps={searchProps}  />);
+    overviewProps={overviewProps}
+    balanceSheetProps={balanceSheetProps}
+    cashFlowProps={cashFlowProps}
+    incomeStatementProps={incomeStatementProps}  />);
   expect(queryByTestId("search-input-test")).toBeTruthy();
   expect(queryByPlaceholderText("Search Stock Ticker")).toBeTruthy();
 });
